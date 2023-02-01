f7a6f3a3e6ba8905535ee267481e2267bd569c86
julian <jpryde44@gmail.com>
2023-02-01 15:40:53 -0500

    Update .pyx file to include ParticleList class

--- swarm_c.pyx
+++ swarm_c.pyx
@@ -1,28 +1,87 @@
 from particle_c import Particle, \
-    find_hypotenuse, \
-    find_particle_distance, \
     SpeedToHighError, \
     LocalRadiusTooSmall
 from input_handling import ArgumentException
 import plot_particles
+from math_functions import find_hypotenuse
 import numpy as np
+import functools
+from typing import Sized
 
 
-class Swarm:
+class ParticleList(Sized):
+    def __init__(self, **kwargs):
+        if "particles" in kwargs:
+            self.particles = kwargs["particles"]
+        else:
+            self.particles = [Particle(kwargs["limits"], i) for i in range(kwargs["num_particles"])]
+
+    def __len__(self) -> int:
+        return len(self.particles)
+
+    def __iter__(self):
+        return iter(self.particles)
+
+    def build_particle_list(self, expression):
+        return [particle for p_index, particle in enumerate(self.particles) if expression(p_index)]
+
+    def __getitem__(self, index):
+        if isinstance(index, (list, np.ndarray)):
+
+            if isinstance(index[0], (bool, np.bool_)):
+                particle_list = self.build_particle_list(lambda particle_index: index[particle_index])
+                return ParticleList(particles=particle_list)
+
+            if isinstance(index[0], (int, np.int_, np.int32, np.int64)):
+                particle_list = self.build_particle_list(lambda particle_index: index == particle_index)
+                return ParticleList(particles=particle_list)
+
+        elif isinstance(index, (int, np.int_, np.int32, np.int64)):
+            return self.particles[index]
+
+    def get_scores(self):
+        return np.array([particle.score for particle in self.particles])
+
+    def get_velocities(self):
+        return np.array([particle.velocity for particle in self.particles])
+
+    def remove(self, particles_to_remove):
+        if isinstance(particles_to_remove, ParticleList):
+            particle_ids = [particle.id for particle in particles_to_remove]
+        else:
+            particle_ids = particles_to_remove.id
+
+        self.particles = self.build_particle_list(lambda particle_index: particle_index not in particle_ids)
+
+    def pop(self, index):
+        return self.particles.pop(index)
+
+    def get_best(self, optimization_function):
+        extreme = np.argmin if optimization_function == "min" else np.argmax
+        scores = self.get_scores()
+
+        return self[extreme(scores)]
+
+    def iterate_particles(self, function, *args):
+        full_function = functools.partial(function, args)
+        output_list = list(map(full_function, self))
+        outputs = np.array(output_list)
+        return outputs
+
+
+class Swarm(ParticleList):
     def __init__(self, swarm_arguments):
         self.limits = swarm_arguments['limits']
+        super().__init__(limits=self.limits, num_particles=swarm_arguments["num_particles"])
         self.initial_local_radius_limit = swarm_arguments['local_radius_limit']
         self.local_radius_limit = self.initial_local_radius_limit
         self.min_local_radius_limit = np.double(0.01)
         self.velocity_update_method = swarm_arguments['velocity_update_method']
-        self.most_movement = 1
-        self.best_particle = None
-        self.best_particle_id = None
+        self.fastest_particle = self[0]
+        self.best_particle = self[0]
         self.previous_best_particle = None
         self.list_of_groups = None
-        self.particle_list = [Particle(self.limits) for i in range(swarm_arguments['num_particles'])]
-        self.r_squareds = np.zeros(len(self.particle_list))
-        self.correlation_coefficient = None
+        self.r_squareds = np.zeros(len(self.particles))
         self.velocity_coefficient = swarm_arguments['velocity_coefficient']
         self.high_particle_velocity_counter = 0
         if 'sigma' in swarm_arguments:
@@ -54,47 +113,44 @@ class Swarm:
                                    self.annealing_lifetime
 
     def call_forcing_function(self):
-        for particle in self.particle_list:
+        for particle in self.particles:
             particle.execute_forcing_function()
 
     def find_local_groups(self):
-        find_local_groups_success = False
-        while not find_local_groups_success:
+        find_local_groups_success = np.bool_([False])
+        while not all(find_local_groups_success):
             try:
-                for particle in self.particle_list:
-                    particle.find_particles_in_local_radius(self)
-                find_local_groups_success = True
+                find_local_groups_success = \
+                    self.iterate_particles(lambda args, particle: particle.find_particles_in_local_radius(self), ())
             except LocalRadiusTooSmall:
                 self.raise_local_radius_limit()
                 find_local_groups_success = False
 
-    def update_swarm_velocities_with_best_neighbor(self, optimization_function, velocity_coefficient):
-        for particle in self.particle_list:
-            particle.find_best_neighbor(self, optimization_function)
-            particle.update_velocity_with_best_neighbor(velocity_coefficient)
+    def update_velocities_with_best_neighbor(self):
+        args = self.velocity_coefficient
+        velocity_coefficient_too_high = any(
+            self.iterate_particles(
+                lambda inner_args, particle: particle.update_velocity_with_best_neighbor(*inner_args), *args
+            )
+        )
+        return velocity_coefficient_too_high
 
-        return velocity_coefficient
+    def update_velocities_with_gradient(self, least_squares_method, optimization_function):
+        args = (self.velocity_coefficient, optimization_function, least_squares_method)
 
-    def update_swarm_velocities_with_gradient(self, optimization_function, velocity_coefficient, least_squares_method):
-        velocity_coefficient_too_high = False
-        for index, particle in enumerate(self.particle_list):
-            velocity_coefficient_too_high, r_squared = \
-                particle.update_velocity_with_gradient(
-                    velocity_coefficient, optimization_function, least_squares_method
-                )
-
-            self.r_squareds[index] = r_squared
-
+        outputs = self.iterate_particles(
+            lambda inner_args, particle: particle.update_velocity_with_gradient(*inner_args), *args
+        )
+        self.r_squareds = outputs[:, 0]
+        velocity_coefficient_too_high = any(outputs[:, 1])
         return velocity_coefficient_too_high
 
     def update_swarm_velocities(self, optimization_function, least_squares_method):
         if self.velocity_update_method == "best neighbor":
-            velocity_coefficient_too_high = \
-                self.update_swarm_velocities_with_best_neighbor(optimization_function, self.velocity_coefficient)
+            velocity_coefficient_too_high = self.update_velocities_with_best_neighbor()
         elif self.velocity_update_method == "gradient":
-            velocity_coefficient_too_high = self.update_swarm_velocities_with_gradient(
-                optimization_function, self.velocity_coefficient, least_squares_method
-            )
+            velocity_coefficient_too_high = self.update_velocities_with_gradient(least_squares_method,
+                                                                                 optimization_function)
         else:
             raise ArgumentException("Velocity update method: \"" + self.velocity_update_method + "\" not implemented.")
 
@@ -106,87 +162,63 @@ class Swarm:
         return np.mean(self.r_squareds)
 
     def move_particles(self):
-        for particle in self.particle_list:
+        for particle in self.particles:
             particle.move()
 
     def add_randomness_factor(self):
-        for particle in self.particle_list:
+        for particle in self.particles:
             particle.shake(self.sigma)
 
-    def find_most_movement(self):
-        self.most_movement = 0
-        particle_movement = 0
-        for index, particle in enumerate(self.particle_list):
-            try:
-                particle_movement = find_hypotenuse(particle.velocity)
-                if particle_movement > 10:
-                    raise SpeedToHighError(particle_movement)
-            except SpeedToHighError as error:
-                print("Particle " + str(index) + "velocity too high at " + str(error.speed) + ". "
-                      "Reducing particle velocity to 0.")
-                particle.velocity = [0] * len(self.limits)
-                self.velocity_coefficient -= 0.001
-                print("Lowering velocity coefficient by 0.001 to: " + str(self.velocity_coefficient))
-                print("This occurrence number: " + str(self.high_particle_velocity_counter))
-            if self.most_movement < particle_movement:
-                self.most_movement = particle_movement
+    def find_fastest_particle(self):
+        particle_movements = np.array(list(map(find_hypotenuse, self.get_velocities())))
+        self.fastest_particle = self[np.argmax(particle_movements)]
+        particle_movements_over_limit = particle_movements > find_hypotenuse(np.ones(len(self.limits)))
+
+        try:
+            if any(particle_movements_over_limit):
+                raise SpeedToHighError(particle_movements)
+        except SpeedToHighError as error:
+            self[particle_movements_over_limit].velocity = np.zeros(len(self.limits))
+            self.velocity_coefficient -= 0.001
+            self.high_particle_velocity_counter += 1
+            print("Particle(s)" + str(np.where(particle_movements_over_limit)) + " velocity too high at " +
+                  str(error.speed) + ". Reducing particle velocity to 0, lowering velocity coefficient by 0.001 to: " +
+                  str(self.velocity_coefficient) + ". This is occurence number: " +
+                  str(self.high_particle_velocity_counter))
 
     def find_best_particle(self, function):
         self.previous_best_particle = self.best_particle
-        self.best_particle = self.particle_list[0]
-        self.best_particle_id = 0
-        for index, particle in enumerate(self.particle_list):
-            if particle.score < self.best_particle.score and function == "min":
-                self.best_particle = particle
-                self.best_particle_id = index
-            elif particle.score > self.best_particle.score and function == "max":
-                self.best_particle = particle
-                self.best_particle_id = index
-
-        if self.best_particle is self.previous_best_particle:
-            return True
-        else:
-            return False
+        self.best_particle = self.get_best(function)
+        return True if self.best_particle is self.previous_best_particle else False
 
     def print_summary(self, iteration):
         output_string = \
             "Iteration: " + str(iteration) + "\n" + \
             "sigma: " + str(self.sigma) + "\n" + \
             "initial sigma: " + str(self.initial_sigma) + "\n" + \
-            "Most movement: " + str(self.most_movement) + "\n" + \
+            "Most movement: " + str(find_hypotenuse(self.fastest_particle.velocity)) + "\n" + \
             "Best Particle Score: " + str(self.best_particle.score) + "\n" + \
             "Best particle position: " + str(self.best_particle.calculate_raw_position()) + "\n" + \
-            "Best particle ID: " + str(self.best_particle_id) + "\n" + \
+            "Best particle ID: " + str(self.best_particle.id) + "\n" + \
             "Best particle velocity: " + str(self.best_particle.velocity) + "\n"
         if self.velocity_update_method == "gradient":
             output_string += "Average R2: " + str(self.r_squareds.mean()) + "\n"
         print(output_string)
 
-    def identify_particles_in_radius(self, base_particle, particles_not_yet_assigned):
-        particles_in_radius = []
-        for index, other_particle in enumerate(particles_not_yet_assigned):
-            if find_particle_distance(other_particle, base_particle) < self.local_radius_limit:
-                particles_in_radius.append(other_particle)
-
-        return particles_in_radius, particles_not_yet_assigned
-
     def iterate_neighbors(self, particles_not_yet_assigned, particles_in_local_group, base_particle):
-        particles_in_radius, particles_not_yet_assigned = self.identify_particles_in_radius(
-            base_particle, particles_not_yet_assigned
-        )
+        particles_not_yet_assigned.remove(base_particle.particles_in_local_radius)
+        particles_in_local_group += base_particle.particles_in_local_radius
 
-        for particle in particles_in_radius:
-            particles_not_yet_assigned.remove(particle)
-        particles_in_local_group += particles_in_radius
-
-        for particle in particles_in_radius:
-            particles_in_local_group = \
-                self.iterate_neighbors(particles_not_yet_assigned, particles_in_local_group, particle)
-
-        return particles_in_local_group
+        if len(particles_not_yet_assigned):
+            args = (particles_not_yet_assigned, particles_in_local_group)
+            self.iterate_particles(
+                lambda inner_args, inner_base_particle: self.iterate_neighbors(*inner_args, inner_base_particle),
+                *args)
+        else:
+            return particles_in_local_group
 
     def find_groups_recursive(self):
-        particles_not_yet_assigned = self.particle_list.copy()
+        particles_not_yet_assigned = ParticleList(particles=self.particles)
         list_of_groups = []
         while len(particles_not_yet_assigned) > 0:
             new_base_particle = particles_not_yet_assigned.pop(0)
@@ -199,5 +231,5 @@ class Swarm:
         self.list_of_groups = list_of_groups
 
     def plot_particle_positions(self):
-        plot = plot_particles.PlotParticles(self.limits, self.particle_list)
+        plot = plot_particles.PlotParticles(self.limits, self.particles)
         plot.plot_particle_positions(plot_contour_overlay=True)
